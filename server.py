6270d354e617fd4278a3076c1141cd3d94029677
DanKee43 <Simex43@yandex.ru>
2023-02-27 02:20:30 +0700

    init

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, json
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_page():
+    return render_template("home.html")
+
+
+@app.errorhandler(404)
+def error404_page(e):
+    return render_template("404_page.html"), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
